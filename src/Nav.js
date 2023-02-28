a23e1fdbe8dde2c933f4c58279c4f4a03e861d5e
Michael <michaeldrpabaya@gmail.com>
2023-02-28 14:15:17 +0800

    navbar

---
+++ src/Nav.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import netflixLogo from './Images/Netflix_Logo_Print_OneColorPMS.webp'
+import netflixAvatar from './Images/0ddccae723d85a703b798a5e682c23c1.png'
+import './Nav.css';
+
+function Nav() {
+
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                handleShow(true)
+            } else {
+                handleShow(false)
+            }
+
+            return () => {
+                window.removeEventListener("scroll");
+            }
+        })
+    }, [])
+
+    return (
+        <div className={`nav ${show && 'nav__black'}`}>
+            <img className="nav__logo"
+                src={netflixLogo}
+                alt='Netflix Logo'
+            />
+            <img className="nav__avatar"
+                src={netflixAvatar}
+                alt='Netflix Avatar'
+            />
+        </div>
+    )
+}
+
+export default Nav;
